683387094bc26ca70f5b33d12b3f9701dabf24c5
Savannah Younts <savyounts@gmail.com>
2018-09-12 18:03:47 +0000

    automatically backed up by learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,15 +1,14 @@
 class Artist < ActiveRecord::Base
-  # include Slugify::InstanceMethods
-  # extend Slugify::ClassMethods
+  # include SlugifyableInstanceMethods
+  # extend SlugifyableClassMethods
   has_many :songs
   has_many :genres, through: :songs
 
   def slug
-    song_name = name.gsub(" ", "-").downcase
+    self.name.gsub(" ", "-").downcase
   end
 
-  def self.find_by_slug(slug)
-    artist_name = slug.split("-").map(&:capitalize).join(' ')
-    artist = self.find_by(name: artist_name)
+  def find_by_slug(slug) 
+    self.all.find{ |instance| instance.slug == slug } 
   end
 end
