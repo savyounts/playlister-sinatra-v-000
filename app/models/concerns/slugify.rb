70e016587093837fa3b7aa3b5dee61274a681112
Savannah Younts <savyounts@gmail.com>
2018-09-12 18:12:52 +0000

    automatically backed up by learn

--- app/models/concerns/slugify.rb
+++ app/models/concerns/slugify.rb
@@ -1,9 +1,5 @@
 
-#   module SlugifyableInstanceMethods
-#     def slug (filename)
-#       song_name = filename.gsub(/^(.*) - /, "").gsub(/\[([^\]]*)\].*/, "")
-#     end
-#   end
+#  
 #
 #   module SlugifyableClassMethods
 #     def find_by_slug(slug) 
