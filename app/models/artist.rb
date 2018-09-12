7364d96979e9a1a1be2d3639e0c38049ec9e75c2
Savannah Younts <savyounts@gmail.com>
2018-09-12 18:08:19 +0000

    automatically backed up by learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -8,7 +8,7 @@ class Artist < ActiveRecord::Base
     self.name.gsub(" ", "-").downcase
   end
 
-  def find_by_slug(slug) 
+  def self.find_by_slug(slug) 
     self.all.find{ |instance| instance.slug == slug } 
   end
 end
