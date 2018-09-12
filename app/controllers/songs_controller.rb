be4776dbf190f3dfc64c788f64d0d8c622a6ff1e
Savannah Younts <savyounts@gmail.com>
2018-09-12 19:23:55 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -32,7 +32,7 @@ class SongsController < ApplicationController
   patch '/songs/:slug' do
     @song = Song.find_by_slug(params[:slug])
     @song.name = params[:name],
-    @song.artist = Artist.find_or_create_by(params[:artist_name])
+    @song.artist = Artist.find_or_create_by(name: params[:artist_name])
     @song.genres = params[:genres]
     @song.save
 
