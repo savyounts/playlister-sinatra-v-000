f76401c65ca06eba9f7024d31366c14046110535
Savannah Younts <savyounts@gmail.com>
2018-09-12 18:43:36 +0000

    automatically backed up by learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -7,7 +7,7 @@ class ArtistsController < ApplicationController
 
   get '/artists/:slug' do
     @artist = Artist.find_by_slug(params[:slug])
-    @songs = @artist.songs 
+    @songs = @artist.songs
     @genres = @artist.genres
     erb :'artists/show'
   end
